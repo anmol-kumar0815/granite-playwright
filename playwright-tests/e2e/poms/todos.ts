551c03fc446f5452b40eb02769e4ccf6c26304e0
Anmol Kumar <anmol.kumar@bigbinary.com>
2023-12-20 15:00:38 +0530

    Added all tests for tasks and multi tab test for role based test

--- playwright-tests/e2e/poms/todos.ts
+++ playwright-tests/e2e/poms/todos.ts
@@ -1,5 +1,13 @@
 import { Page, expect } from "@playwright/test";
 
+interface TodoName {
+  todoName: string;
+}
+
+interface CreateNewTodoProps extends TodoName {
+  userName?: string;
+}
+
 export class TodoPage {
   page: Page;
 
@@ -7,11 +15,11 @@ export class TodoPage {
     this.page = page;
   }
 
-  createTodoAndVerify = async ({ newTodo }: { newTodo: string }) => {
+  createTodoAndVerify = async ({ newTodo, userName = "Oliver Smith", }: CreateNewTodoProps) => {
     await this.page.getByTestId("navbar-add-todo-link").click();
     await this.page.getByTestId("form-title-field").fill(newTodo);
     await this.page.locator(".css-2b097c-container").click();
-    await this.page.locator(".css-26l3qy-menu").getByText("Oliver Smith").click();
+    await this.page.locator(".css-26l3qy-menu").getByText(userName).click();
     await this.page.getByTestId("form-submit-button").click();
     const taskInDashboard = this.page
       .getByTestId("tasks-pending-table")
@@ -20,5 +28,30 @@ export class TodoPage {
       });
     await taskInDashboard.scrollIntoViewIfNeeded();
     await expect(taskInDashboard).toBeVisible();
-  }
+  };
+
+  markTodoAsCompletedAndVerify = async ({ newTodo }: { newTodo: string }) => {
+    await this.page
+      .getByTestId("tasks-pending-table")
+      .getByRole("row", { name: newTodo })
+      .getByRole("checkbox")
+      .click();
+    const completedTaskInDashboard = this.page
+      .getByTestId("tasks-completed-table")
+      .getByRole("row", { name: newTodo });
+    await completedTaskInDashboard.scrollIntoViewIfNeeded();
+    await expect(completedTaskInDashboard).toBeVisible();
+  };
+
+  starTodoAndVerify = async ({ newTodo }: TodoName) => {
+    const starIcon = this.page
+      .getByTestId("tasks-pending-table")
+      .getByRole("row", { name: newTodo })
+      .getByTestId("pending-task-star-or-unstar-link");
+    await starIcon.click();
+    await expect(starIcon).toHaveClass(/ri-star-fill/i);
+    await expect(
+      this.page.getByTestId("tasks-pending-table").getByRole("row").nth(1)
+    ).toContainText(newTodo);
+  };
 }
--- playwright-tests/e2e/tests/todos.spec.ts
+++ playwright-tests/e2e/tests/todos.spec.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { test } from "../fixtures";
 import { expect } from '@playwright/test';
+import LoginPage from '../poms/login';
 
 test.describe("Todo page", () => {
   let newTodo: string;
@@ -21,15 +22,87 @@ test.describe("Todo page", () => {
   }) => {
     await page.goto("/");
     await todoPage.createTodoAndVerify({ newTodo });
-    await page
-      .getByTestId("tasks-pending-table")
-      .getByRole("row", { name: newTodo })
-      .getByRole("checkbox")
-      .click();
+    await todoPage.markTodoAsCompletedAndVerify({ newTodo });
+  });
+
+  test("should be able to delete a completed todo", async ({
+    page,
+    todoPage,
+  }) => {
+    await page.goto("/");
+    await todoPage.createTodoAndVerify({ newTodo });
+    await todoPage.markTodoAsCompletedAndVerify({ newTodo });
     const completedTaskInDashboard = page
       .getByTestId("tasks-completed-table")
       .getByRole("row", { name: newTodo });
-    await completedTaskInDashboard.scrollIntoViewIfNeeded();
-    await expect(completedTaskInDashboard).toBeVisible();
+
+    await completedTaskInDashboard
+      .getByTestId("completed-task-delete-link")
+      .click();
+
+    await expect(completedTaskInDashboard).toBeHidden();
+    await expect(
+      page
+        .getByTestId("tasks-pending-table")
+        .getByRole("row", { name: newTodo })
+    ).toBeHidden();
+  });
+
+  test.describe("Starring tasks feature", () => {
+    test.describe.configure({ mode: "serial" });
+
+    test("should be able to star a pending todo", async ({ page, todoPage }) => {
+      page.goto("/");
+      await todoPage.createTodoAndVerify({ newTodo });
+      await todoPage.starTodoAndVerify({ newTodo });
+    });
+  });
+
+  test("should be able to un-star a pending todo", async ({
+    page,
+    todoPage,
+  }) => {
+    page.goto("/");
+    await todoPage.createTodoAndVerify({ newTodo });
+    await todoPage.starTodoAndVerify({ newTodo });
+    const starIcon = page
+      .getByTestId("tasks-pending-table")
+      .getByRole("row", { name: newTodo })
+      .getByTestId("pending-task-star-or-unstar-link");
+    await starIcon.click();
+    await expect(starIcon).toHaveClass(/ri-star-line/);
+  });
+
+  test("should create a new todo with a different user as the assignee", async ({
+    page,
+    browser,
+    todoPage,
+  }) => {
+    await page.goto("/");
+    await todoPage.createTodoAndVerify({ newTodo, userName: "Sam Smith" });
+
+    // Creating a new browser context and a page in the browser without restoring the session
+    const newUserContext = await browser.newContext({
+      storageState: { cookies: [], origins: [] },
+    });
+    const newUserPage = await newUserContext.newPage();
+
+    // Initializing the login POM here because the fixture is configured to use the default page context
+    const loginPage = new LoginPage(newUserPage);
+
+    await newUserPage.goto("/");
+    await loginPage.loginAndVerifyUser({
+      email: "sam@example.com",
+      password: "welcome",
+      username: "Sam Smith",
+    });
+    await expect(
+      newUserPage
+        .getByTestId("tasks-pending-table")
+        .getByRole("row", { name: newTodo })
+    ).toBeVisible();
+
+    await newUserPage.close();
+    await newUserContext.close();
   });
 });
